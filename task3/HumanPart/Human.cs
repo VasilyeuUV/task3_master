858aeac18c2b0496b69093471c77dd1ff702a01a
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2019-12-07 16:15:36 +0300

    Human part base info

---
+++ task3/HumanPart/Human.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using task3.Interfaces;
+
+namespace task3.HumanPart
+{
+    public class Human
+    {
+        /// <summary>
+        /// Some human personal information
+        /// </summary>
+        IEnumerable<IPersonalData> _personalDataList = null;
+
+
+        IEnumerable<IPBXStatusable> _pBXStatuses = null;
+
+    }
+}
---
+++ task3/Interfaces/IPersonalData.cs
@@ -0,0 +1,15 @@
+﻿namespace task3.Interfaces
+{
+
+    /// <summary>
+    /// Human personal data interface
+    /// </summary>
+    interface IPersonalData
+    {
+        /// <summary>
+        /// Get some personal information
+        /// </summary>
+        /// <returns></returns>
+        string GetInfo();
+    }
+}
